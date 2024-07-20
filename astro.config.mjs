42123dc1bf339f2636080b2101d44405b8315e50
Dhruvan <dhruvan.k05@gmail.com>
2024-07-21 00:34:34 +0100

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,55 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import axios from 'axios';
+
+async function fetchGrammar() {
+    const url = 'https://raw.githubusercontent.com/kneorain/helix-highlighter/main/syntaxes/helix.tmLanguage.json';
+    const response = await axios.get(url);
+    return response.data;
+}
+
+const grammar = await fetchGrammar();
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
+			title: 'Helix',
+
+            logo: {
+                src: "/public/favicon.svg",
+                replacesTitle: false,
+            },
+            editLink: {
+                baseUrl: 'https://github.com/kneorain/helix-api-docs/edit/main/src/content/docs',
+            },
+            social: {
+                github: 'https://github.com/kneorain/helix',
+            },
+            customCss: ["/src/styles/docstyle.css"],
+            expressiveCode: {
+                themes: ['one-dark-pro', 'one-light'],
+                shiki: {
+                    langs: [
+                        grammar
+                    ],
+                },
+                styleOverrides: { borderRadius: '0.2rem' },
+            },
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Internal Reference',
+					badge: 'New',
+					autogenerate: { directory: 'internal' },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Toolchain API',
+					badge: 'Wip',
+					autogenerate: { directory: 'toolchain' },
 				},
 			],
 		}),
 	],
 });
+
+
